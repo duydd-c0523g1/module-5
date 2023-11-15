2cbcb03b74a1bebc3ed13c077be03630902a8b09
duydd-c0623g1 <daoducduy.work@gmail.com>
2023-11-15 13:59:29 +0700

    update

--- module-5-react-app/src/main.jsx
+++ module-5-react-app/src/main.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./student.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const students = [
+  {
+    company: "Alfreds Futterkiste",
+    contact: "Maria Anders",
+    country: "Germany",
+  },
+  {
+    company: "Centro comercial Moctezuma",
+    contact: "Francisco Chang",
+    country: "Mexico",
+  },
+  {
+    company: "Ernst Handel",
+    contact: "Roland Mendel",
+    country: "Austria",
+  },
+  {
+    company: "Island Trading",
+    contact: "Helen Bennett",
+    country: "UK",
+  },
+  {
+    company: "Laughing Bacchus Winecellars",
+    contact: "Yoshi Tannamuri",
+    country: "Canada",
+  },
+  {
+    company: "Magazzini Alimentari Riuniti",
+    contact: "Giovanni Rovelli",
+    country: "Italy",
+  },
+];
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    {students.map((student) => (
+      <tr>
+        <td>{student.company}</td>
+        <td>{student.contact}</td>
+        <td>{student.country}</td>
+      </tr>
+    ))}
+  </>
+);
