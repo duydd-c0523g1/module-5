5869cef4d94959d190d2c7850acc98adb8e15957
duydd-c0623g1 <daoducduy.work@gmail.com>
2023-11-20 14:32:00 +0700

    Update ContactForm.jsx

--- ss05_01/src/components/ContactForm.jsx
+++ ss05_01/src/components/ContactForm.jsx
@@ -7,16 +7,38 @@ function ContactForm() {
     email: "",
     phone: "",
   };
+  
   const validateObject = {
     fullName: Yup.string().required("Tên không được để trống"),
-    email: Yup.string().required("Email không được để trống")
+    email: Yup.string().required("Email không được để trống").matches(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, "Sai định dạng"),
+    phone: Yup.string().required("Số điện thoại không được để trống").matches(/^\+?[0-9]{10,12}$/, "Sai định dạng")
   }
 
   return (
-    <div>
+    <>
       <h1>Contact Form</h1>
-      <Formik initialValues={initValues}></Formik>
-    </div>
+      <Formik initialValues={initValues}
+      validationSchema={Yup.object(validateObject)}>
+        <Form>
+            <div>
+            <label htmlFor="fullName">Name</label>
+            <Field type='text' id='fullName' name='fullName'/>
+            <ErrorMessage name="fullName" component='span' />
+            </div>
+            <div>
+            <label htmlFor="email">Email</label>
+            <Field type='email' id='fullName' name='email'/>
+            <ErrorMessage name="email" component='span' />
+            </div>
+            <div>
+            <label htmlFor="phone">Phone</label>
+            <Field type='tel' id='phone' name='phone'/>
+            <ErrorMessage name="phone" component='span' />
+            </div>
+            <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </>
   );
 }
 export default ContactForm;
